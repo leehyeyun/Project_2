9cf1db04d6ae6323e96e8d09001568592dad1873
dlgpdbs <dlgpdbs12@naver.com>
2022-09-22 16:43:17 +0900

    [test commit]

---
+++ src/main/java/com/sist/dao/FoodDAO.java
@@ -0,0 +1,59 @@
+package com.sist.dao;
+
+import org.apache.ibatis.annotations.Select;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+import com.sist.mapper.*;
+import com.sist.vo.*;
+@Repository
+public class FoodDAO {
+  @Autowired
+  private FoodMapper mapper;
+  
+  /*
+   *   @Select("SELECT cno,title,subject,poster FROM food_category")
+       public List<CategoryVO> foodCategoryAllData();
+   */
+  public List<CategoryVO> foodCategoryAllData()
+  {
+	  return mapper.foodCategoryAllData();
+  }
+  /*
+   *   @Select("SELECT fno,cno,name,tel,poster,type FROM food_house "
+		  +"WHERE cno=#{cno}")
+       public FoodVO foodCategoryListData(int cno);
+   */
+  public List<FoodVO> foodCategoryListData(int cno)
+  {
+	  return mapper.foodCategoryListData(cno);
+  }
+  /*
+   *   @Select("SELECT title,subject FROM food_category WHERE cno=#{cno}")
+       public CategoryVO categoryInfoData(int cno);
+   */
+  public CategoryVO categoryInfoData(int cno)
+  {
+	  return mapper.categoryInfoData(cno);// JSP전송 (Controller) => DispatcherServlet에서 제어할 수 없다 
+  }
+  
+  /*
+   *   @Select("SELECT poster,rownum FROM recipe "
+		  +"WHERE REGEXP_LIKE(title,#{type}) "
+		  +"AND rownum<=12")
+       public List<String> foodLikeRecipe(String type);
+   */
+  public List<String> foodLikeRecipe(String type)
+  {
+	  return mapper.foodLikeRecipe(type);
+  }
+  /*
+   *  @Select("SELECT * FROM food_house "
+		  +"WHERE fno=#{fno}")
+      public FoodVO foodDetailData(int fno);
+   */
+  public FoodVO foodDetailData(int fno)
+  {
+	  return mapper.foodDetailData(fno);
+  }
+}
---
+++ src/main/java/com/sist/web/MemberController.java
@@ -0,0 +1,157 @@
+package com.sist.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.sist.dao.*;
+import com.sist.vo.*;
+// 커스터마이징 , 리팩토링  
+@Controller
+public class MemberController {
+   @Autowired
+   private MemberDAO dao;
+   
+   @Autowired
+   private BCryptPasswordEncoder encoder;
+   
+   @GetMapping("member/join.do")
+   public String member_join(Model model)
+   {
+	   model.addAttribute("main_jsp", "../member/join.jsp");
+	   return "main/main";//forward => request를 전송 
+   }
+   
+   @PostMapping("member/join_ok.do")
+   public String member_join_ok(MemberVO vo)
+   {
+	   vo.setTel(vo.getTel1()+"-"+vo.getTel2());
+	   String en=encoder.encode(vo.getPwd());// 암호화 
+	   //encoder.encode(vo.getId())
+	   vo.setPwd(en);
+       dao.memberJoinInsert(vo);	   
+	   return "redirect:../main/main.do"; //재전송 (sendRedirect()) => request초기화 
+   }
+   
+   @PostMapping("member/idcheck.do")
+   @ResponseBody
+   // JSP파일명 , .do 전송 ==> 일반 데이터, JSON ==> @RestController
+   public String member_idcheck(String id)
+   {
+	   String result="";
+	   int count=dao.memberIdCheck(id);
+	   if(count==0)
+	   {
+		   result="YES";
+	   }
+	   else
+	   {
+		   result="NO";
+	   }
+	   return result;
+   }
+   
+   @GetMapping("member/login.do")
+   public String member_login(Model model)
+   {
+	   model.addAttribute("main_jsp", "../member/login.jsp");
+	   return "main/main";
+   }
+   
+   @PostMapping("member/login_ok.do")
+   @ResponseBody
+   public String member_login_ok(String id,String pwd,boolean ck,HttpSession session,HttpServletResponse response)
+   {
+	   String result="";
+	   int count=dao.memberIdCheck(id);
+	   if(count==0)
+	   {
+		   result="NOID";
+	   }
+	   else
+	   {
+		   MemberVO vo=dao.memberJoinInfoData(id);
+		   if(encoder.matches(pwd, vo.getPwd()))// 암호된 비밀번호 / 일반 비밀번호 비교 
+		   {
+			   session.setAttribute("id", id);
+			   session.setAttribute("name", vo.getName());
+			   session.setAttribute("role", vo.getRole());
+			   if(ck==true)
+			   {
+				   Cookie cookie=new Cookie("id", id);
+				   cookie.setPath("/");
+				   cookie.setMaxAge(60*60*24);
+				   response.addCookie(cookie);
+				   ////////////////////////////////
+				   cookie=new Cookie("name", vo.getName());
+				   cookie.setPath("/");
+				   cookie.setMaxAge(60*60*24);
+				   response.addCookie(cookie);
+				   ///////////////////////////////
+				   cookie=new Cookie("role", vo.getRole());
+				   cookie.setPath("/");
+				   cookie.setMaxAge(60*60*24);
+				   response.addCookie(cookie);
+			   }
+			   result="OK";
+		   }
+		   else
+		   {
+			   result="NOPWD";
+		   }
+	   }
+	   return result;
+   }
+   
+   @GetMapping("member/logout.do")
+   public String member_logout(HttpSession session)
+   {
+	   session.invalidate();
+	   return "redirect:../main/main.do";
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
